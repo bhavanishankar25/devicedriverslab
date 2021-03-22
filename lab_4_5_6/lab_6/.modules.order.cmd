d0cc79e862946382fa71cecfc91599e446313590
bhavanishankar25 <47072061+bhavanishankar25@users.noreply.github.com>
2021-03-22 16:16:56 +0530

    CED17I031

---
+++ lab_4_5_6/lab_6/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/home/bhavani/ddlab/lab2/lab6/modules.order := {   echo /home/bhavani/ddlab/lab2/lab6/simple_char_drv.ko; :; } | awk '!x[$$0]++' - > /home/bhavani/ddlab/lab2/lab6/modules.order
---
+++ lab_4_5_6/lab_6/.simple_char_drv.mod.cmd
@@ -0,0 +1,1 @@
+cmd_/home/bhavani/ddlab/lab2/lab6/simple_char_drv.mod := { echo  /home/bhavani/ddlab/lab2/lab6/simple_char_drv.o;  echo; } > /home/bhavani/ddlab/lab2/lab6/simple_char_drv.mod
