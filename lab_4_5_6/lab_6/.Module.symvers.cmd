95f612d393e1378faa3b67ae3a84c5a52d4102e8
bhavanishankar25 <47072061+bhavanishankar25@users.noreply.github.com>
2021-03-22 16:14:21 +0530

    .

--- lab_4_5_6/lab_6/.Module.symvers.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bhavani/ddlab/lab2/lab6/Module.symvers := sed 's/ko$$/o/' /home/bhavani/ddlab/lab2/lab6/modules.order | scripts/mod/modpost -m -a   -o /home/bhavani/ddlab/lab2/lab6/Module.symvers -e -i Module.symvers   -T -
--- lab_4_5_6/lab_6/simple_char_drv.mod
+++
@@ -1,2 +0,0 @@
-/home/bhavani/ddlab/lab2/lab6/simple_char_drv.o
-
