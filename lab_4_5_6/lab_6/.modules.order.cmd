5da0327f4e20634a2e643008f41aa819a9d29bd2
bhavanishankar25 <47072061+bhavanishankar25@users.noreply.github.com>
2021-03-22 21:43:32 +0530

    Delete lab_4_5_6/lab_6 directory

--- lab_4_5_6/lab_6/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bhavani/ddlab/lab2/lab6/modules.order := {   echo /home/bhavani/ddlab/lab2/lab6/simple_char_drv.ko; :; } | awk '!x[$$0]++' - > /home/bhavani/ddlab/lab2/lab6/modules.order
--- lab_4_5_6/lab_6/.simple_char_drv.mod.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/bhavani/ddlab/lab2/lab6/simple_char_drv.mod := { echo  /home/bhavani/ddlab/lab2/lab6/simple_char_drv.o;  echo; } > /home/bhavani/ddlab/lab2/lab6/simple_char_drv.mod
